9d59acbc393522a2e395b11c71f112d98bd96e51
timothy1205 <timothy120500@gmail.com>
2024-01-06 14:51:20 -0700

    Grab live subscriptions for homepage

--- web/src/app/pages/home-page/home-page.component.ts
+++ web/src/app/pages/home-page/home-page.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
+import { SubscriptonService } from './../../servies/subscripton.service';
+import { Component, OnInit } from '@angular/core';
 import { SubscriptonComponent } from '../../components/subscripton/subscripton.component';
+import { SubscriptionModel } from '../../models/SubscriptionModel';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [SubscriptonComponent],
+  imports: [SubscriptonComponent, NgFor],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
-export class HomePageComponent {
-  subscriptionTest = {
-    id: 1,
-    name: 'Test Subscription',
-    price: 5,
-    frequency: 1,
-    cycle: 1,
-    subscriptionCategoryId: 1,
-    nextPaymentDate: new Date(),
-  };
+export class HomePageComponent implements OnInit {
+  constructor(private subscriptionService: SubscriptonService) {}
+
+  subscriptions: SubscriptionModel[] = [];
+
+  async ngOnInit() {
+    this.subscriptions = await this.subscriptionService.getSubscriptions();
+  }
 }
